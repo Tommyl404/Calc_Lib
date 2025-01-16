655ec988e6e7cadd6ba4c0f0ff0fbc79a60ee7ee
Tommy Levi <68539909+Tommyl404@users.noreply.github.com>
2025-01-16 20:10:24 +0200

    Update Calc_Mean.m

--- Calc_Mean.m
+++ Calc_Mean.m
@@ -1,10 +1,7 @@
 %input: a - vector of size Tx1
 %output: result - the mean of the vector that is an avarge of the elements.
 function result = Calc_Mean(a)
-T = size(a,1); %size of a in the 1 dim (a vector that looks like Tx1)
-sum_vector = 0; %init to the sum
-for i = 1:T
-sum_vector = sum_vector + a(i,1);
-end
-result = sum_vector / T;
+T = size(a, 1);       % Number of elements in the vector
+sum_vector = sum(a);  % Use MATLAB's sum function to compute the sum
+result = sum_vector / T;  % Divide by the number of elements to get the mean
 end
